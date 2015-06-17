81de9c4421902b742b5a2187321ce20a1b47d811
angadsingh <angad@angadsingh.in>
2015-06-17 06:42:57 +0000

    Don't write metrics when no rule filter matches

--- hraven-etl/src/main/java/com/twitter/hraven/mapreduce/GraphiteHistoryWriter.java
+++ hraven-etl/src/main/java/com/twitter/hraven/mapreduce/GraphiteHistoryWriter.java
@@ -261,8 +261,6 @@ public class GraphiteHistoryWriter {
       
       if (!ruleMatched) {
         incrementCounter("NO_RULE_MATCHED", 1);
-//        Expression exp = getParsedExpression(defaultRule, true);
-//        metricsPath = exp.getValue(context, String.class);
         LOG.warn("Defaulting to default metric path naming rule for app " + recordCollection.getKey().toString());
       }
       
@@ -292,12 +290,10 @@ public class GraphiteHistoryWriter {
     int lineCount = 0;
     
     StringBuilder lines = new StringBuilder();
+    String metricsPath = sinkConfig.getGraphitePrefix() + "." + getMetricsPath();
     
-    if (filterApp()) {
-      incrementCounter(Counters.APPS_FILTERED_IN);
-      
-      String metricsPath = sinkConfig.getGraphitePrefix() + "." + getMetricsPath();
-      
+    if (filterApp() && metricsPath != null) {
+      incrementCounter(Counters.APPS_FILTERED_IN);  
       LOG.info("Sending metrics for: "+ metricsPath);
       try {
         storeAppIdMapping(metricsPath);
@@ -362,14 +358,13 @@ public class GraphiteHistoryWriter {
         lines.append(metricsPath + ".").append(runTimeKey + " " + (finishTime-launchTime) + " " + timestamp + "\n");
         lineCount++;
       }
+      
+      incrementCounter(Counters.METRICS_WRITTEN, lineCount);
+      LOG.info("SendToGraphite: " + recordCollection.getKey().toString() + " : " + lines + " metrics"  + "(config: " + sinkConfig.getName() + ")");
+      socketWriter.write(lines.toString());
     } else {
       incrementCounter(Counters.APPS_FILTERED_OUT);
     }
-    
-    incrementCounter(Counters.METRICS_WRITTEN, lineCount);
-    
-    LOG.info("SendToGraphite: " + recordCollection.getKey().toString() + " : " + lines + " metrics"  + "(config: " + sinkConfig.getName() + ")");
-    socketWriter.write(lines.toString());
   }
 
   private void storeAppIdMapping(String metricsPathPrefix) throws IOException {
